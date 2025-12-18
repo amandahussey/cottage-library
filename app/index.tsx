e761c564d7619e448d0f2a6276d7dd8006ee386a
Amanda Hussey <amanda.k.hussey@gmail.com>
2025-12-18 15:38:39 -0700

    set up tamagui defaultConfig

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,21 @@
-import { Text, View } from "react-native";
+import { defaultConfig } from "@tamagui/config/v4";
+import { createTamagui, TamaguiProvider } from "@tamagui/core";
+import { Button } from "tamagui";
+
+// you usually export this from a tamagui.config.ts file
+const config = createTamagui(defaultConfig);
+
+type Conf = typeof config;
+
+// make imports typed
+declare module "@tamagui/core" {
+  interface TamaguiCustomConfig extends Conf {}
+}
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <TamaguiProvider config={config}>
+      <Button theme="blue">Hello world</Button>
+    </TamaguiProvider>
   );
 }
